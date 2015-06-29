808fff03653c6ca48501bda0f9f12474fd9c386d
Manfred Touron <m@42.am>
2015-06-29 22:44:43 +0200

    Executing script

--- daemon.go
+++ daemon.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
-import "github.com/stianeikeland/go-rpio"
-import "time"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/stianeikeland/go-rpio"
+)
 
 func main() {
 	fmt.Println("Initializing...")
@@ -18,6 +23,17 @@ func main() {
 
 	for {
 		fmt.Printf("Result: %v\n", pin.Read())
+		if pin.Read() == 1 {
+			cmd := exec.Command("/etc/gpio-trigger")
+			err := cmd.Start()
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Waiting for command to finish...\n")
+			err = cmd.Wait()
+			fmt.Printf("Command finished with error: %v\n", err)
+			os.Exit(1)
+		}
 		time.Sleep(time.Second / 5)
 	}
 }
