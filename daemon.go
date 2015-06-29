0ef5d780989c777b165423165ccc8bfbf58dbc2b
Manfred Touron <m@42.am>
2015-06-29 22:33:28 +0200

    Printing when button is pressed

--- daemon.go
+++ daemon.go
@@ -2,9 +2,22 @@ package main
 
 import "fmt"
 import "github.com/stianeikeland/go-rpio"
+import "time"
 
 func main() {
 	fmt.Println("Initializing...")
 	err := rpio.Open()
-	fmt.Println(err)
+	if err != nil {
+		panic(err)
+	}
+	defer rpio.Close()	
+	fmt.Println("Initialized !")
+
+	pin := rpio.Pin(10)
+	pin.Input()
+
+	for {
+		fmt.Printf("Result: %v\n", pin.Read())
+		time.Sleep(time.Second / 5)
+	}
 }
